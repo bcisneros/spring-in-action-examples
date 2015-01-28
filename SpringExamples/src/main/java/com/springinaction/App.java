b3d6cdaf378e0c03397020bcdb1e146717de0817
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2015-01-28 11:24:24 -0700

    Add a PoeticJuggler performer that receives a Poem reference

--- SpringExamples/src/main/java/com/springinaction/App.java
+++ SpringExamples/src/main/java/com/springinaction/App.java
@@ -7,7 +7,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-idol.xml");
+        System.out.println("A juggler instance");
         Performer performer = (Performer) context.getBean("duke");
+        performer.perform();
+
+        System.out.println("A poetic juggler instance");
+        performer = (Performer) context.getBean("poeticDuke");
         performer.perform();
     }
 }
