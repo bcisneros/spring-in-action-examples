da7fb3af5f7748c1c0fdfd52f3a6cb2f54bbc6d0
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2015-01-28 10:55:01 -0700

    Add basic bean configuration

--- SpringExamples/src/main/java/com/springinaction/App.java
+++ SpringExamples/src/main/java/com/springinaction/App.java
@@ -1,13 +1,13 @@
 package com.springinaction;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.springinaction.springidol.Performer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring-idol.xml");
+        Performer performer = (Performer) context.getBean("duke");
+        performer.perform();
     }
 }
